d3ce9a638d841c0309db7a83bf60e165b2fe9543
Pedro Hurtado <pedrohurt@gmail.com>
2025-09-30 18:59:27 +0200

    tema2

---
+++ dia-2/modules/main.js
@@ -0,0 +1,10 @@
+import {suma,multiplicacion,division,resta as r} from './operaciones.js'
+import * as op from './operaciones.js'
+import './run.js'
+
+console.log(r(4,4))
+console.log(multiplicacion(3,5))
+console.log(division(4,4))
+console.log(suma(3,5))
+
+console.log(op.resta(0,0))
---
+++ dia-2/modules/run.js
@@ -0,0 +1,9 @@
+!function foo(a){
+    console.log(`a:${a}`)
+    console.log("Hello Foo")
+}(1)
+
+
+/*(function bar(){
+    console.log("Hello Foo")
+})()*/
